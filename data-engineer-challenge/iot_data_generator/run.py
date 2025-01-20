f09cb747200a5feaef0796b1a5c2cb5a5c71fa44
Justin VNX <justinvnx@outlook.com>
2025-01-20 12:07:34 +0100

    .

--- data-engineer-challenge/iot_data_generator/run.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from generator import Generator
-
-if __name__ == "__main__":
-    generator = Generator()
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(generator.generate())
